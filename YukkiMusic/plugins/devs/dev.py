cfec7d37608d382a4637959c9d119e78630847e6
INNOCENT VIR <102230621+VIR99@users.noreply.github.com>
2022-10-29 16:07:09 +0530

    Update dev.py

--- YukkiMusic/plugins/devs/dev.py
+++ YukkiMusic/plugins/devs/dev.py
@@ -44,7 +44,7 @@ async def edit_or_reply(msg: Message, **kwargs):
 
 @app.on_message(
     filters.command("eval")
-    & None
+    & ~filters.user(OWNER_ID)
     & ~filters.forwarded
     & ~filters.via_bot
 )
@@ -152,7 +152,7 @@ async def forceclose_command(_, CallbackQuery):
 
 @app.on_message(
     filters.command("sh")
-    & None
+    & ~filters.user(OWNER_ID)
     & ~filters.forwarded
     & ~filters.via_bot
 )
