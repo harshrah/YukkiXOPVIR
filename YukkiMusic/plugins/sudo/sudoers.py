faeaf21d2a4f7118aca34b03b28efe1283abc494
INNOCENT VIR <102230621+VIR99@users.noreply.github.com>
2022-11-23 15:22:40 +0530

    Update sudoers.py

--- YukkiMusic/plugins/sudo/sudoers.py
+++ YukkiMusic/plugins/sudo/sudoers.py
@@ -105,7 +105,7 @@ async def userdel(client, message: Message, _):
     await message.reply_text(f"Something wrong happened.")
 
 
-@app.on_message(filters.command(SUDOUSERS_COMMAND) & filters.user(OWNER_ID) ~BANNED_USERS)
+@app.on_message(filters.command(SUDOUSERS_COMMAND) & filters.user(OWNER_ID) )
 @language
 async def sudoers_list(client, message: Message, _):
     text = _["sudo_5"]
