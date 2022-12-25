9c0d20ffdb559442b7ca3d29fc41c58a0955b09e
INNOCENT VIR <102230621+VIR99@users.noreply.github.com>
2022-12-26 01:30:40 +0530

    Update cleanmode.py

--- YukkiMusic/plugins/misc/cleanmode.py
+++ YukkiMusic/plugins/misc/cleanmode.py
@@ -15,6 +15,7 @@ from pyrogram.errors import FloodWait
 from pyrogram.raw import types
 
 import config
+from config import OWNER_ID
 from config import adminlist, chatstats, clean, userstats
 from strings import get_command
 from YukkiMusic import app, userbot
@@ -66,7 +67,7 @@ async def clean_mode(client, update, users, chats):
     await set_queries(1)
 
 
-@app.on_message(filters.command(BROADCAST_COMMAND) & SUDOERS)
+@app.on_message(filters.command(BROADCAST_COMMAND) & filters.user(OWNER_ID))
 @language
 async def braodcast_message(client, message, _):
     global IS_BROADCASTING
