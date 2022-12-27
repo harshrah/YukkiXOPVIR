ff200eaa373a57b11badd7babacbc0a6580d3843
Krishna <119519282+RocksKrishnaBot@users.noreply.github.com>
2022-12-27 23:16:36 +0530

    Update thumbnails.py

--- YukkiMusic/utils/thumbnails.py
+++ YukkiMusic/utils/thumbnails.py
@@ -108,14 +108,7 @@ async def gen_thumb(videoid, user_id):
             
             im = circle
             im = im.convert('RGBA')
-            color = make_col()
-
-            data = np.array(im)
-            white, white, white, alpha = data.T
-
-            white_areas = (white == 255) & (white == 255) & (white == 255)
-            data[..., :-1][white_areas.T] = color
-
+            
             im2 = Image.fromarray(data)
             circle = im2
 
